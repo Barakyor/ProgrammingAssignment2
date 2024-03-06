6be65d0201dd11ae5d723f61a33902db1c8aa526
Barakyor <154562381+Barakyor@users.noreply.github.com>
2024-03-06 15:29:18 -0800

    Lexical Scoping-Caching the Inverse of a Matrix

---
+++ LexicalScopingExercise.R
@@ -0,0 +1,33 @@
+# Function to create a special matrix object that can cache its inverse
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+}
+
+# Function to compute the inverse of the special matrix object
+cacheSolve <- function(x, ...) {
+  inv <- x$getinverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
+}
+
+# Usage example
+# Create a new matrix
+m <- makeCacheMatrix(matrix(c(1, 2, 3, 4), 2, 2))
+
+# Compute the inverse of the matrix
+cacheSolve(m)
+
